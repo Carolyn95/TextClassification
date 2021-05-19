915f9b8a18ff7fe1d8ca742adb97b9286a497983
Carolyn Chen <yanchongcarolynchen@gmail.com>
2021-05-19 16:38:54 +0800

    assertCountEqual not assertEqual

--- tests/test_parse_data.py
+++ tests/test_parse_data.py
@@ -1,21 +1,22 @@
 import unittest
 import sys
-sys.path.insert(1, '/Users/carolyn/MyGit/NLP/TextClassification/')
+sys.path.append('/Users/carolyn/MyGit/NLP/TextClassification/')
 from parse_data import data_xml_to_df
 
 
 class TestParseData(unittest.TestCase):
 
   def setUp(self):
-    self.data_file = "data/ABSA16_Restaurants_Train_SB1_v2.xml"
+    self.data_file = "../data/ABSA16_Restaurants_Train_SB1_v2.xml"
 
   def test_xml2df(self):
     parsed_df = data_xml_to_df(self.data_file)
-    all_columns = parsed_df.columns
-    self.assertEqual(all_columns, [
-        'opinion_id', 'text_content', 'target', 'category', 'polarity',
-        'ote_start', 'ote_stop'
-    ])
+    all_columns = [c for c in parsed_df.columns]
+    parsed_columns = [
+        'opinion_id', 'text', 'target', 'category', 'polarity', 'ote_start',
+        'ote_stop'
+    ]
+    self.assertCountEqual(all_columns, parsed_columns)
 
 
 if __name__ == '__main__':
