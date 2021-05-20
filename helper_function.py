133357b19c8140715de63d53a3dd5116b0380989
Carolyn Chen <yanchongcarolynchen@gmail.com>
2021-05-20 18:04:17 +0800

    word2vec to embed text

--- helper_function.py
+++ helper_function.py
@@ -84,6 +84,7 @@ def process(*dfs):
   """
   lemmatizer = WordNetLemmatizer()
   for df in dfs:
+    print(f'Before dataframe length {len(df)}')
     for i, row in df.iterrows():
       # pdb.set_trace()
       text = row['text']
@@ -104,4 +105,82 @@ def process(*dfs):
       text_lemmatized = ' '.join(
           [lemmatizer.lemmatize(word, pos='v') for word in text_tokenized])
       row['text'] = text_lemmatized
+    # remove empty sentences after processing (eg. original text only contains digits)
+    df = df.loc[df['text'].apply(len) != 0]
+    print(f'After dataframe length {len(df)}')
   return dfs
+
+
+def createMappings(*dfs):
+  counts = Counter()
+  df_all = pd.concat(dfs)
+  for text in df_all['text']:
+    text = text.split(' ')
+    counts.update(text)
+  # sort word counts, descending frequency
+  sorted_counts = sorted(counts, key=counts.get, reverse=True)
+  # data exploration
+  helper_iter = [(0, 10, 1, '*** Top 10 Most Frequent Words ***'),
+                 (-1, -11, -1, '*** Bottom 10 Most Frequent Words ***')]
+  for it in helper_iter:
+    print(it[3])
+    for i in range(it[0], it[1], it[2]):
+      word = sorted_counts[i]
+      line = ['Word:', word, 'Occurences', counts[word]]
+      print('{:<0} {:<16} {:<0} {:<0}'.format(*line))
+    print('\n')
+
+  # create word to index dictionary, mapping unique word to integer
+  word_to_index = {
+      word: index for index, word in enumerate(sorted_counts, start=2)
+  }
+  # add padding token to 1st position of the vocab
+  word_to_index['<PAD>'] = 0
+  # add OOV (out-of-vocabulary) token to 2nd position of the vocab
+  word_to_index['<OOV>'] = 1
+  # create index to word dictionary, mapping integer to unique word
+  index_to_word = {v: k for k, v in word_to_index.items()}
+  return word_to_index, index_to_word
+
+
+def getEmbeddingMatrix(model_path, word_to_index, vocab_size=None):
+  """Get pre-trained GloVe embedding for every word.
+     If word not in pre-trained vocabulary, will return a random numeric vector to represent.
+
+     Args:
+      model_path: path to pre-trained word2vec model 
+      word_to_index: dictionary of {word:index}
+      vocab_size: default to length of word_to_index
+     Returns:
+      embedding_matrix: an embedding matrix of words in target text set
+  """
+  # load embeddings in a dictionary
+  embeddings = {}
+  with open(model_path, 'r', encoding='utf-8') as file:
+    for line in file:
+      values = line.strip().split()
+      word = values[0]
+      vector = np.asarray(values[1:], dtype='float32')
+      embeddings[word] = vector
+  # randomly initialize the embedding matrix (vocab_size * embedding_size)
+  vocab_size = vocab_size if vocab_size else len(word_to_index)
+  embedding_size = len(list(embeddings.values())[0])
+  np.random.seed(2021)
+  embedding_matrix = np.random.uniform(-1, 1, size=(vocab_size, embedding_size))
+
+  # replace row with corresponding embedding vector if a given word is within the embedding vocab
+  num_loaded = 0
+  for word, index in word_to_index.items():
+    vec = embeddings.get(word)
+    if vec is not None and index < vocab_size:
+      embedding_matrix[index] = vec
+      num_loaded += 1
+
+  # finish and print how many words were loaded
+  print('Successfully loaded pretrained embeddings for %f/%f words.' %
+        (num_loaded, vocab_size))
+  print(
+      'Embedding vectors for non-loaded words were randomly initialized between -1 and 1.'
+  )
+  embedding_matrix = embedding_matrix.astype(np.float32)
+  return embedding_matrix
--- train.py
+++ train.py
@@ -4,17 +4,26 @@ from helper_function import *
 train_file = 'data/ABSA16_Restaurants_Train_SB1_v2.xml'
 test_file = 'data/restaurants_trial_english_sl.xml'
 
-df_train = data_xml_to_df(train_file)
-df_test = data_xml_to_df(test_file)
-
 if __name__ == '__main__':
-  # df_test = replaceNA(df_test)
-  # df_train = replaceNA(df_train)
-  # df_test = splitEntityAspect(df_test)
-  # df_test = splitEntityAspect(df_test)
+  df_train = data_xml_to_df(train_file)
+  df_test = data_xml_to_df(test_file)
   df_test = preprocess(df_test)
   df_train = preprocess(df_train)
   explore(df_train, df_test)
   print(len(df_train), len(df_test))
   df_new_train, df_new_test = process(df_train, df_test)
   print(df_new_train.loc[42, 'text'])
+  word_to_index, index_to_word = createMappings(df_train, df_test)
+
+  # GloVe 100d model
+  embedding_matrix = getEmbeddingMatrix(model_path='GloVe.6B/glove.6B.100d.txt',
+                                        word_to_index=word_to_index)
+  print('Embedding Matrix Shape:', embedding_matrix.shape)
+
+  # define vocab_size and embedding_size variables for later use
+  vocab_size = embedding_matrix.shape[0]
+  embedding_size = embedding_matrix.shape[1]
+  print('Vocabulary Size:', vocab_size)
+  print('Embedding Size:', embedding_size)
+  pdb.set_trace()
+  print()
